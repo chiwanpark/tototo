c02ca4e3204d29d100024d94466d15dfc05fc744
Chiwan Park <chiwanpark@icloud.com>
2015-01-29 12:20:51 +0900

    Modify layout of index page

--- tototo/endpoints/index.py
+++ tototo/endpoints/index.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timezone
 from flask import Blueprint, render_template
+from sqlalchemy import desc
 from tototo import db_session
 from tototo.auth import get_current_user
 from tototo.database import Meeting
 
 context = Blueprint('index', __name__)
 
+
 @context.route('/')
 def get_index():
     meeting_count = db_session.query(Meeting).filter(Meeting.when < datetime.now(tz=timezone.utc)).count()
-    return render_template('index.html', meeting_count=meeting_count, current_user=get_current_user())
+    next_meeting = db_session.query(Meeting).filter(Meeting.available).order_by(desc(Meeting.registered)).first()
+    return render_template('index.html', meeting_count=meeting_count, current_user=get_current_user(), next_meeting=next_meeting)
