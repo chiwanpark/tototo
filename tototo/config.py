e80a5b7af8e7a112ab011e1d134d706b80b4e5b7
Chiwan Park <chiwanpark@icloud.com>
2015-04-27 21:20:51 +0900

    Change environment name for database setting

--- tototo/config.py
+++ tototo/config.py
@@ -2,6 +2,6 @@
 import os
 import pytz
 
-DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_RED_URL')
+DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 TIMEZONE = pytz.timezone('Asia/Seoul')
