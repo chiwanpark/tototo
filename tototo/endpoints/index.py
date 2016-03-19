9cfd96cc4e8eef21d325b5bc6296e21c9afa60d2
Chiwan Park <chiwanpark@icloud.com>
2016-03-19 17:20:47 +0900

    Fix bug in fetching next meeting

--- tototo/endpoints/index.py
+++ tototo/endpoints/index.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 from flask import Blueprint, render_template
-from sqlalchemy import desc
+from sqlalchemy import asc
 from tototo import db_session, config
 from tototo.auth import get_current_user
 from tototo.database import Meeting
@@ -14,5 +14,5 @@ context = Blueprint('index', __name__)
 @context.route('/')
 def get_index():
     meeting_count = db_session.query(Meeting).filter(Meeting.when < datetime.now(tz=config.TIMEZONE)).count()
-    next_meeting = db_session.query(Meeting).filter(Meeting.available).order_by(desc(Meeting.registered)).first()
+    next_meeting = db_session.query(Meeting).filter(Meeting.available).order_by(asc(Meeting.registered)).first()
     return render_template('index.html', meeting_count=meeting_count, current_user=get_current_user(), next_meeting=next_meeting)
